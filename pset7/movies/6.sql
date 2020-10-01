358f6d8568d31b2c57a4c18c443a37ecfe56324f
joushiiy <72150381+joushiiy@users.noreply.github.com>
2020-10-01 20:48:42 +0800

    Update 6.sql

--- pset7/movies/6.sql
+++ pset7/movies/6.sql
@@ -1,3 +1,6 @@
+--In 6.sql, write a SQL query to determine the average rating of all movies released in 2012.
+--Your query should output a table with a single column and a single row (plus optional header) containing the average rating.
+
 SELECT AVG(rating)
 FROM ratings
 LEFT JOIN movies ON ratings.movie_id = movies.id
